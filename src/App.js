88f3fe07283a3f33f3b4edf42b4a7357f57b193b
xentoshi <xentoshi@protonmail.com>
2022-06-16 22:36:44 +0200

    🐉 Added the Model

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 import React, { Suspense, useRef} from 'react';
 import { Canvas } from "@react-three/fiber";
+import { Html, useGLTF } from '@react-three/drei'
 import './App.css';
 
 function Model (props) {
   const group = useRef()
-
-
+  // Load the model
+  const { nodes, materials } = useGLTF("/mac-draco.glb");
+  // Make it float here
   return (
     <group ref={group} {...props} dispose={null}>
-
+      <group rotation-x={-0.425} position={[0, -0.04, 0.41]}>
+        <group position={[0, 2.96, -0.13]} rotation={[Math.PI / 2, 0, 0]}>
+          <mesh material={materials.aluminium} geometry={nodes['Cube008'].geometry} />
+          <mesh material={materials['matte.001']} geometry={nodes['Cube008_1'].geometry} />
+          <mesh geometry={nodes['Cube008_2'].geometry}>
+            {/* Drei's HTML component can now "hide behind" canvas geometry */}
+            <Html className="content" rotation-x={-Math.PI / 2} position={[0, 0.05, -0.09]} transform occlude>
+              <div className="wrapper">
+                {/** Here comes the page*/}
+              </div>
+            </Html>
+          </mesh>
+        </group>
+      </group>
+      <mesh material={materials.keys} geometry={nodes.keyboard.geometry} position={[1.79, 0, 3.45]} />
+      <group position={[0, -0.1, 3.39]}>
+        <mesh material={materials.aluminium} geometry={nodes['Cube002'].geometry} />
+        <mesh material={materials.trackpad} geometry={nodes['Cube002_1'].geometry} />
+      </group>
+      <mesh material={materials.touchbar} geometry={nodes.touchbar.geometry} position={[0, -0.03, 1.2]} />
     </group>
   )
 }
