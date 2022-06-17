a0229f5de0219230c53d045ef79827b264912f43
xentoshi <xentoshi@protonmail.com>
2022-06-17 21:38:22 +0200

    🥷🏽 Added Shadow and Orbit Controls

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import * as THREE from 'three'
 import React, { Suspense, useRef} from 'react';
 import { Canvas, useFrame } from '@react-three/fiber';
-import { Html, useGLTF } from '@react-three/drei'
+import { Html, useGLTF, ContactShadows, OrbitControls } from '@react-three/drei'
 import './App.css';
 
 function Model (props) {
@@ -15,7 +15,6 @@ function Model (props) {
     group.current.rotation.y = THREE.MathUtils.lerp(group.current.rotation.y, Math.sin(t / 4) / 10, 0.1)
     group.current.rotation.z = THREE.MathUtils.lerp(group.current.rotation.z, Math.cos(t / 4) / 20, 0.1 )
     group.current.rotation.y = THREE.MathUtils.lerp(group.current.rotation.y, (-5 + Math.sin(t)) / 5, 0.1)
-
   })
   return (
     <group ref={group} {...props} dispose={null}>
@@ -53,6 +52,8 @@ export default function App() {
           <Model />
         </group>
       </Suspense>
+      <ContactShadows rotation-x={Math.PI / 2} position={[0, -4.5, 0]} opacity={1} width={20} height={20} blur={1} far={4.5} /> 
+      <OrbitControls enablePan={false} enableZoom={false} minPolarAngle={Math.PI / 2} maxPolaxAngle={Math.PI / 2} />
     </Canvas>
   );
 }
