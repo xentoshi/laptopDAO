22818e1e2a5fc94cc775fc73761f8ecb9ec7bdda
xentoshi <xentoshi@protonmail.com>
2022-07-18 23:47:30 +0200

    Design changes

--- src/Page.js
+++ src/Page.js
@@ -1,30 +1,38 @@
 
 const REALMS = "https://realms.today/dao/laptopDAO"
-const PIKA = "https://media3.giphy.com/media/slVWEctHZKvWU/giphy.gif?cid=ecf05e47i3advtf7c6ui7u11afaqgxo3a95781220w42eve7&rid=giphy.gif&ct=g"
 const FORM = "https://airtable.com/shrG4Ovb6HqQAZt19"
 const DRCMDA = "https://github.com/drcmda"
-const TWITTER_LINK = "https://twitter.com/laptopDAO"
+const discordInvite = "https://discord.gg/yGkzajsC";
+
+
+const twitterHandle = "laptopDAO";
+const twitterLink = `https://twitter.com/${twitterHandle}`;
 
 export default function Example() {
   return (
     <div>
       <div className="main">
-      <div className="logo">laptopDAO</div>
-        <img src={PIKA} alt="pika" width="315px" />
-        <div className="text">
-          <div>
-            <button className="signup-button">
+        <div className="logo">laptopDAO</div>
+        <button className="signup-button">
               <a href={FORM} target="_blank" rel="noopener noreferrer"> 
-              ⚡apply for laptop⚡
+              ⚡ apply ⚡
               </a>
-              </button></div>
-        <br></br>
-         🌴 follow us on <a href={TWITTER_LINK} target="_blank" rel="noopener noreferrer">twitter</a>
-        <br></br>
-        🏄‍♂️ coming soon on <a href={REALMS} target="_blank" rel="noopener noreferrer">solana</a>
+        </button>
+        <button className="signup-button">
+              <a href={REALMS} target="_blank" rel="noopener noreferrer"> 
+              🎁 dao 🎁
+              </a>
+        </button>
         </div>
-        <div className="footer"> model created in react three fiber by <a href={DRCMDA} target="_blank" rel="noopener noreferrer">@drcmda</a></div>
+            {/*Socials*/}
+            <a href={twitterLink} target="_blank" rel="noreferrer">
+            <img alt="Twitter Logo" className="twitter-logo" src="twitter-logo.svg" />
+            </a>
+            <a href={discordInvite} target="_blank" rel="noreferrer">
+            <img alt="Discord Logo" className="discord-logo" src="discord-logo.svg" />
+            </a>
+
+        <div className="footer"> laptop model created by <a href={DRCMDA} target="_blank" rel="noopener noreferrer">@drcmda</a></div>
       </div>
-  </div>
   )
 }
