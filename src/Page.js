4f661acf977739e74f40cf469b5b914b7403c739
xentoshi <xentoshi@protonmail.com>
2022-07-19 12:00:01 +0200

    Design updates

--- src/Page.js
+++ src/Page.js
@@ -5,6 +5,7 @@ const DRCMDA = "https://github.com/drcmda"
 const discordInvite = "https://discord.gg/yGkzajsC";
 
 
+
 const twitterHandle = "laptopDAO";
 const twitterLink = `https://twitter.com/${twitterHandle}`;
 
@@ -12,17 +13,17 @@ export default function Example() {
   return (
     <div>
       <div className="main">
-        <div className="logo">laptopDAO</div>
-        <button className="signup-button">
+        <div className="logo">(o˘◡˘o)	</div>
               <a href={FORM} target="_blank" rel="noopener noreferrer"> 
+              <button className="signup-button">
               ⚡ apply ⚡
+              </button>
               </a>
-        </button>
-        <button className="signup-button">
               <a href={REALMS} target="_blank" rel="noopener noreferrer"> 
+              <button className="signup-button">
               🎁 dao 🎁
+              </button>
               </a>
-        </button>
         </div>
             {/*Socials*/}
             <a href={twitterLink} target="_blank" rel="noreferrer">
@@ -31,7 +32,7 @@ export default function Example() {
             <a href={discordInvite} target="_blank" rel="noreferrer">
             <img alt="Discord Logo" className="discord-logo" src="discord-logo.svg" />
             </a>
-
+            <iframe title="pika" src="https://giphy.com/embed/67KsrjDg2bUWI5uyvB" width="140" height="140" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
         <div className="footer"> laptop model created by <a href={DRCMDA} target="_blank" rel="noopener noreferrer">@drcmda</a></div>
       </div>
   )
