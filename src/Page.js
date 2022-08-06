764655343189bfc5196dcf86ee812910aa7e085a
xentoshi <xentoshi@protonmail.com>
2022-08-06 19:04:17 +0200

    Update link

--- src/Page.js
+++ src/Page.js
@@ -3,7 +3,7 @@ const docs = "https://laptopdao.notion.site/laptopDAO-3c90d35d918f4172bebbd8d303
 const form = "https://airtable.com/shrG4Ovb6HqQAZt19"
 const drcmda = "https://github.com/drcmda"
 const pika = "https://giphy.com/embed/67KsrjDg2bUWI5uyvB"
-const discordInvite = "https://discord.gg/yGkzajsC";
+const discordInvite = "https://discord.gg/n9jcxqwe";
 
 const twitterHandle = "laptopDAO";
 const twitterLink = `https://twitter.com/${twitterHandle}`;
