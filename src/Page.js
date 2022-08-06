90cdefa2f299363f66adbc2439ce9ac2b6f89493
xentoshi <xentoshi@protonmail.com>
2022-08-06 21:53:03 +0200

    Link update

--- src/Page.js
+++ src/Page.js
@@ -1,6 +1,5 @@
 
 const docs = "https://laptopdao.notion.site/laptopDAO-3c90d35d918f4172bebbd8d3034b312c"
-const form = "https://airtable.com/shrG4Ovb6HqQAZt19"
 const drcmda = "https://github.com/drcmda"
 const pika = "https://giphy.com/embed/67KsrjDg2bUWI5uyvB"
 const discordInvite = "https://discord.gg/n9jcxqwe";
