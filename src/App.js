e00a17789f02e093f65da4d1adbaee6f0c375fca
xentoshi <xentoshi@protonmail.com>
2022-12-23 18:28:35 +0100

    Added background image

--- src/App.js
+++ src/App.js
@@ -3,10 +3,15 @@ import CountdownTimer from './components/CountdownTimer/CountdownTimer';
 
 function App() {
   return (
-    <div className="App">
-     <CountdownTimer
-     countdownTimestampMs={1671818400000}/>
-    </div>
+    <>
+      <div className="App">
+
+        <CountdownTimer
+      countdownTimestampMs={1671991200000}/>
+      </div>
+
+
+    </>
   );
 }
 
