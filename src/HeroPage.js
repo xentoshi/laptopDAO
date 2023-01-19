360d1efd4a32cb25413c8608ca84a7fead93ce18
xentoshi <xentoshi@protonmail.com>
2023-01-19 20:22:10 +0900

    Added docs link & CTA & more text

--- src/HeroPage.js
+++ src/HeroPage.js
@@ -12,7 +12,7 @@ function App() {
     <div>
       <header>
         <div>
-            <h1 className="logo">laptopDAO</h1>
+            <h1 className="logo">laptopDAO </h1>
               <div className="links">
                 <a href={DISCORD_INVITE} target="_blank" rel="noopener noreferrer">  👾 </a>
                 <a href={TWITTER_LINK} target="_blank" rel="noopener noreferrer">  🐦  </a>
@@ -22,14 +22,20 @@ function App() {
       </header>
       <container>
       <h2>Buy a laptop for a kid</h2>
-      <h3> ↓ <br></br>What is laptopDAO?</h3> 
-      <p> laptopDAO is a group of friends connecting curious kids who cannot afford a laptop to those who can get them one. </p>
+      <p>Provide a grant for a laptop & be part of the chain of happiness </p>
+      <a href={DISCORD_INVITE} target="_blank" rel="noopener noreferrer">
+      <button class="button-35" role="button">Join us ( ^_^)／</button>
+      </a>
+      <h3>↓</h3>
+      <h3>What is laptopDAO?</h3> 
+      <p> laptopDAO is a group of friends connecting curious kids who cannot afford a laptop to those who can get them one. We are providing grants for laptops to proven builders from our community. </p>
           <br></br>
+      <h3>How we do it?</h3> 
+      <p>This is a young non-profit, structure is yet to be developed. For trust reasons, we do things manually. You can review our <a href={DOCS_LINK} target="_blank" rel="noopener noreferrer">documentation</a> for more info. </p>
           <h3> 🎁 
           <br></br>
           Donors </h3>
           <DonorsList/>
-          <p>Join us on <a href={DISCORD_INVITE} target="_blank" rel="noopener noreferrer"> discord</a> and <a href={TWITTER_LINK} target="_blank" rel="noopener noreferrer"> twitter</a> and let's talk there.  </p>
           <br></br>
           <h3>
              🧸 
@@ -38,7 +44,11 @@ function App() {
           <p>Magic can happen when there is someone believing in you. </p>
           <h3>💞</h3>
          <FAQ />
-         <h3>🍀</h3>
+         <h3>
+            <a href={DISCORD_INVITE} target="_blank" rel="noopener noreferrer"> 👾 </a> 
+            <a href={TWITTER_LINK} target="_blank" rel="noopener noreferrer"> 🐦 </a>
+            <a href={DOCS_LINK} target="_blank" rel="noopener noreferrer">  📝  </a>
+         </h3>
       </container>
     </div>
     
